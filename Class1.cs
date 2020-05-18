6522b9530a656404cc6d62e5b0df9b1d8ec8b18c
Đào Thanh Tùng <tung.dao@techx.vn>
2020-05-18 17:32:07 +0700

    TRX-107 runable excel

---
+++ Class1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace RtdServer
+{
+    [
+        ComVisible(true),
+        Guid("dcc6eaaf-f6c9-46f3-8cb5-bfab118e6099"),
+        ProgId("hello_rtd_excel"),
+    ]
+    public class RTDDemoServer : IRtdServer
+    {
+        private IRTDUpdateEvent _callback;
+        private Timer _timer;
+        private Random _rng;
+        private string _symbol;
+
+        public RTDDemoServer()
+        {
+            _rng = new Random();
+        }
+
+        public int ServerStart(IRTDUpdateEvent CallbackObject)
+        {
+            _callback = CallbackObject;
+            _timer = new Timer();
+            _timer.Elapsed += new ElapsedEventHandler(job);
+            _timer.Interval = 1000;
+            return 1;
+        }
+
+        private void job(object sender, EventArgs args) { _callback.UpdateNotify(); }
+
+        public void ServerTerminate()
+        {
+            if (_timer != null)
+            {
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
+
+        public int Heartbeat() { return 1; }
+
+        public dynamic ConnectData(int topicId, ref Array strings, ref bool GetNewValues)
+        {
+            _symbol = strings.GetValue(0).ToString();
+            _timer.Start();
+            return getData(_symbol);
+        }
+
+        public void DisconnectData(int topicId) { }
+
+        public Array RefreshData(ref int TopicCount)
+        {
+            object[,] data = new object[10, 10];
+            for (int i = 0; i < 4; i++)
+            {
+                data[0, i] = i;
+                data[1, i] = getData(_symbol);
+                TopicCount = i + 1;
+            }
+            _timer.Start();
+            return data;
+        }
+
+        private string getData(string symbol)
+        {
+            try { return "data from hello_rtd_excel_0: " + _rng.Next(0, 100).ToString(); }
+            catch (Exception ex) { return "error when getData: " + ex.Message; }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+﻿# Excel RTD demo
+Create a DLL for receiving real time data in Excel.  
+
+
+### Steps
+* Create new project C# Class Library .Net Framework in [VS2019](https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&rel=16)
+* Install NuGet package Microsoft.Office.Interop.Excel
+* Implements interface Microsoft.Office.Interop.Excel.IRtdServer
+* Project > Properties > Signing: sign the assembly
+* Build the solution, output is a DLL in `bin\Debug`
+* Run admin cmd:  
+`C:\Windows\Microsoft.NET\Framework64\v4.0.30319>RegAsm.exe PathToBuiltDLL /codebase`  
+* The Excel file TestRTD.xlsx should receiving real time data
