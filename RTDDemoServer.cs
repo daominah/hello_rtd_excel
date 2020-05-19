0fd6c39772e0020dd52563022bb3ff20573ecd10
Đào Thanh Tùng <tung.dao@techx.vn>
2020-05-19 13:31:08 +0700

    API-430 return data by topic

--- RTDDemoServer.cs
+++ RTDDemoServer.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Office.Interop.Excel;
-using System;
+﻿using System;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Timers;
+using System.Collections.Generic;
+
+using Microsoft.Office.Interop.Excel;
 using Newtonsoft.Json;
 
 namespace RtdServer
@@ -17,7 +19,8 @@ namespace RtdServer
         private IRTDUpdateEvent _callback;
         private Timer _timer;
         private Random _rng;
-        private string _topic;
+        // map topicId to topicName
+        private Dictionary<int, string> _topics;
 
         public RTDDemoServer()
         {
@@ -42,10 +45,18 @@ namespace RtdServer
             _timer.Interval = 1000;
             _timer.Start();
             _rng = new Random();
+            _topics = new Dictionary<int, string>();
             return 1;
         }
 
         private void job(object sender, EventArgs args) { _callback.UpdateNotify(); }
+
+        // Called by Excel if a given interval has elapsed since the last time
+        // Excel was notified of updates from the RealTimeData server
+        public int Heartbeat() { return 1; }
+
+        // Called whenever Excel no longer requires a specific topic
+        public void DisconnectData(int topicId) { }
 
         // Called when Excel no longer requires RTD topics from the RealTimeData server
         public void ServerTerminate()
@@ -57,20 +68,16 @@ namespace RtdServer
             }
         }
 
-        // Called by Excel if a given interval has elapsed since the last time
-        // Excel was notified of updates from the RealTimeData server
-        public int Heartbeat() { return 1; }
-
         // Called whenever Excel requests a new RTD topic from the RealTimeData server
         public dynamic ConnectData(int topicId, ref Array strings, ref bool GetNewValues)
         {
-            Console.WriteLine("ConnectData topicId: {0}, Array: {1}", topicId, JsonConvert.SerializeObject(strings));
-            _topic = strings.GetValue(0).ToString();
-            return getData(_topic);
+            Console.WriteLine("{0} ConnectData topicId: {1}, strings: {2}",
+                now(), topicId, json(strings));
+            string topicName = strings.GetValue(0).ToString();
+            _topics[topicId] = topicName;
+            Console.WriteLine("_topics: {0}", json(_topics));
+            return getData(topicName);
         }
-
-        // Called whenever Excel no longer requires a specific topic
-        public void DisconnectData(int topicId) { }
 
         // Called when Excel is requesting a refresh on topics.
         // RefreshData will be called after an UpdateNotify has been issued by the server.
@@ -79,23 +86,23 @@ namespace RtdServer
         // - return a two dimensional variant array containing the topic ids and the new values of each.
         public Array RefreshData(ref int TopicCount)
         {
-            object[,] data = new object[10, 10];
-            for (int i = 0; i < 4; i++)
+            object[,] data = new object[_topics.Count, _topics.Count];
+            TopicCount = _topics.Count;
+            for (int i = 1; i <= _topics.Count; i++)
             {
-                data[0, i] = i;
-                data[1, i] = getData(_topic);
-                TopicCount = i + 1;
+                data[0, i - 1] = i;
+                data[1, i - 1] = getData(_topics[i]);
             }
-            _timer.Start();
+            Console.WriteLine("{0} RefreshData: {1}", now(), json(data));
             return data;
         }
 
-        private string getData(string symbol)
+        private string getData(string topic)
         {
-            try { return "data from hello_rtd_excel_0: " + _rng.Next(0, 100).ToString(); }
-            catch (Exception ex) { return "error when getData: " + ex.Message; }
+            return String.Format("data from hello_rtd_excel for topic {0} at {1} {2}", topic, now(), _rng.Next(0, 100));
         }
 
         private string now() { return DateTime.UtcNow.ToString("o") + ": "; }
+        private string json(object obj) { return JsonConvert.SerializeObject(obj); }
     }
 }
